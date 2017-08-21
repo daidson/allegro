661a656dd2f829baa4db22bc2cd8a24160a9b30c
Daidson Fonseca Alves <daidson.alves@gmail.com>
2017-08-21 10:06:08 -0300

    alteracoes no triangulo

--- ex1.c
+++ ex1.c
@@ -71,11 +71,10 @@ int main(void)
     /* circle(buff, 160, 120, 100, CORAMARELO);
     textprintf_ex(buff, font, 50, 50, CORVERDE, CORPRETO, "Teste do circulo."); */
     
-    /* triangle(buff, 100, 160, 120, 120, 160, 100, CORVERDE); */
-
 /*    triangle(buff, 10, 10, 10, 50, 50, 50, CORVERDE);
     triangle(buff, 50, 50, 50, 90, 90, 90, CORVERDE); */
-    triangle(buff, 150, 100, 100, 150, 50, 50, CORVERDE);
+
+    triangle(buff, 1, 1, 1, 1.5, 5, 5, CORVERDE);
 
     save_bitmap(IMAGENAME, buff, pal);
     destroy_bitmap(buff);
